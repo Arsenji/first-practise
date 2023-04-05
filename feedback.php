d45e167a060e3627807262726a6fadab47752101
Arsen <Arsen.Berdnikov@mail.ru>
2023-04-05 10:38:05 +0300

    created 3 pages. Authentication, greetings and feedback.Also created ccs styles.

--- feedback.php
+++ feedback.php
@@ -1,39 +1,72 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    define( 'DB_HOST', 'localhost');
+    define( 'DB_USER', 'root');
+    define( 'DB_PASS', '');
+    define( 'DB_NAME', 'feedback');
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME );
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
+$textShort = mysqli_real_escape_string($conn, $_POST['textShort']);
+$textareaField = mysqli_real_escape_string($conn, $_POST['textareaField']);
+$radioButtons = mysqli_real_escape_string($conn, $_POST['radioButtons']);
+$selectedField = mysqli_real_escape_string($conn, $_POST['selectField']);
+$checkBox = mysqli_real_escape_string($conn, $_POST['checkbox']);
+
+$sql = "INSERT INTO feedback_users (textShort, textareaField, radioButtons, selectField, checkBox) VALUES ('$textShort', '$textareaField', '$radioButtons', '$selectedField','$checkBox')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Данные успешно сохранены";
+} else {
+    echo "Ошибка: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="feedback.css" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="feedback.css" />
     <title>Обратная связь</title>
 </head>
 <body>
-	<form id="form">
-		<label name="label1">Текстовое поле:</label>
-		<input type="text"><br>
+	<form id="form" method="post">
+		<label for="textShort">Оставьте краткий отзыв:</label>
+		<input type="text" name="textShort"><br>
 		
-		<label for="textarea_field">Многострочное текстовое поле:</label><br>
-		<textarea id="textarea_field" name="textarea_field" rows="4" cols="50"></textarea><br>
+		<label for="textareaField">Оставьте полный отзыв:</label><br>
+		<textarea name="textareaField" rows="4" cols="50"></textarea><br>
 		
-		<label>Радиокнопки:</label><br>
-		<input type="radio" id="radio_button_1" name="radio_buttons" value="1">
-		<label for="radio_button_1">Первая радиокнопка</label><br>
-		<input type="radio" id="radio_button_2" name="radio_buttons" value="2">
-		<label for="radio_button_2">Вторая радиокнопка</label><br>
+		<label for="radioButtons">Понравился ли вам наш сайт?:</label><br>
+		<input type="radio" id="radioButton_1" name="radioButtons" value="1">
+		<label for="radioButton_1">Понравился</label><br>
+		<input type="radio" id="radioButton_2" name="radioButtons" value="2">
+		<label for="radioButton_2">Очень понравился</label><br>
 
-		<label for="select_field">Выпадающий список:</label>
-		<select id="select_field" name="select_field">
-			<option value="1">Первый вариант</option>
-			<option value="2">Второй вариант</option>
-			<option value="3">Третий вариант</option>
+		<label for="selectField">Что бы вы добавили?</label>
+		<select id="selectField" name="selectField">
+			<option value="1">Ничего</option>
+			<option value="2">Сайт идеален</option>
+			<option value="3">Мне все равно</option>
 		</select><br>
 
-		<label>Флажки:</label><br>
-		<input type="checkbox" id="checkbox_1" name="checkbox_1" value="1">
-		<label for="checkbox_1">Первый флажок</label><br>
-		<input type="checkbox" id="checkbox_2" name="checkbox_2" value="2">
-		<label for="checkbox_2">Второй флажок</label><br>
+		<label>Насколько бы вы оценили наш сайт:</label><br>
+		<input type="checkbox" id="checkbox_1" name="checkbox" value="1">
+		<label for="checkbox_1">1</label><br>
+		<input type="checkbox" id="checkbox_2" name="checkbox" value="2">
+		<label for="checkbox_2">2</label><br>
+        <input type="checkbox" id="checkbox_3" name="checkbox" value="3">
+        <label for="checkbox_1">3</label><br>
+        <input type="checkbox" id="checkbox_4" name="checkbox" value="4">
+        <label for="checkbox_1">4</label><br>
+        <input type="checkbox" id="checkbox_5" name="checkbox" value="5">
+        <label for="checkbox_1">5</label><br>
 
 		<input type="submit" name="Send" id="send" value="Отправить">
-		<input type="submit" name="Delete" id="delete" value="Сбросить">
+		<input type="submit" name="Delete" id="delete" value="Сбросить"><br>
 	</form>
 </body>
 </html>
